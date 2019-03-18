d4b5fba9db3142a8472570e7f1243be74b36ce77
André K. Berger <andre@kbsystem.com.br>
2019-03-18 14:36:36 -0300

    Add files via upload
    Xuxa só para programadores!!! :)

---
+++ ducks.php
@@ -0,0 +1,14 @@
+<?php
+  echo "<h1>XUXA Só para programadores PHP</h1>";
+  for($n = 5; $n >= 1; $n--){
+      echo "$n patinho(s) foram passear ";
+      echo "além das montanhas para brincar ";
+      echo "a mamãe gritou ";
+      for ($x = 1; $x <= 4; $x++){
+        echo "quá! ";
+      }
+      $y = $n-1;
+    echo "mas só $y patinho(s) ";
+    echo "voltaram(ou) de lá :( <br> ";
+  }
+?>
